1fac725b8baa5ac95ed54cfa66419f8beb0da67f
DamirGaleyev <damirgal55@gmail.com>
2023-09-07 08:01:28 +0500

    Дз сделано

--- src/main/java/ru/otus/java/basic/homework1/Homework.java
+++ src/main/java/ru/otus/java/basic/homework1/Homework.java
@@ -2,6 +2,48 @@ package ru.otus.java.basic.homework1;
 
 public class Homework {
     public static void main(String[] args) {
-        System.out.println("Привет");
+        greetings();
+        checkSign(1, 2, 3);
+        selectColor(10);
+        compareNumbers(1,5);
+
     }
+
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+
+    public static void checkSign(int a, int b, int c) {
+        int sum = a + b + c;
+        if (sum > 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void selectColor(int data) {
+
+        if (data <= 10) {
+            System.out.println("Красный");
+        } else if (data > 10 && data <= 20) {
+            System.out.println("Желтый");
+        } else if (data > 20) {
+            System.out.println("Зеленый");
+        } else {
+            System.out.println("Цвета закончились");
+        }
+    }
+    public static void compareNumbers(int a, int b) {
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+
+    }
+
 }
