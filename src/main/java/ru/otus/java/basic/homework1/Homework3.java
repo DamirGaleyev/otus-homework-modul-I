224295460b93871163703f2abf82bc9d6cd8d4c8
DamirGaleyev <damirgal55@gmail.com>
2023-09-23 13:15:54 +0500

    Домашнее задание № 3

---
+++ src/main/java/ru/otus/java/basic/homework1/Homework3.java
@@ -0,0 +1,76 @@
+package ru.otus.java.basic.homework1;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        sumPositiveElements(1, 2, 3, 4, 5, 0, -1, -2, -3, -4, -5);  // 1 пункт ДЗ
+
+        draw2D(5);                                                  // 2 пункт ДЗ
+
+        int[][] array = {{1, 2, 3, 4}, {1, 2, 3, 4}, {3, 5, 5, 6}, {4, 6, 7, 8}};  // 3 пункт ДЗ
+        touchingLine(array);
+        System.out.println();
+
+
+        int maxValue = findMax(array); // 4 пункт ДЗ
+        System.out.println(maxValue + "\n");
+
+        int sum = sumSecondLine(array); // 5 пункт ДЗ
+        System.out.println(sum);
+    }
+
+    public static void sumPositiveElements(int... array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 0) {
+                sum += array[i];
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static void draw2D(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void touchingLine(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {        // обходим каждый элемент массива
+            for (int j = 0; j < arr[i].length; j++) { // обходим каждый элемент массива
+                if (Math.abs(j - i) < 1) {
+                    System.out.print(0 + " ");
+                } else {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static int findMax(int[][] array) {
+        int max = array[0][0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array[i].length; j++) {
+                if (array[i][j] > max) {
+                    max = array[i][j];
+                }
+            }
+        }
+        return max;
+    }
+
+    public static int sumSecondLine(int[][] array) {
+        int sum = 0;
+        if (array.length > 1 && array[1].length > 0) {
+            for (int j = 0; j < array[1].length; j++) {
+                sum += array[1][j];
+            }
+            return sum;
+        }
+        return -1;
+    }
+}
